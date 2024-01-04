d7f8c1330bdd8fee7cb24fc422befc405581fa85
SelL1402 <gfd.lah@gmail.com>
2024-01-04 13:21:27 +0200

    Robo-school
    This is my very first project that I did. The layout was taken from open sources and did not contain animations of the following types: hover, click animation, etc. Also, the layout did not have an adaptive part.

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById("modal__link").addEventListener("click",function(){
+  document.getElementById("my__modal").classList.add("open")
+})
+document.getElementById("close__modal").addEventListener("click",function(){
+  document.getElementById("my__modal").classList.remove("open")
+})
